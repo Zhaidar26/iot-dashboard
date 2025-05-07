056dd5c3232b64991ba74219dc8090e8a1853803
Zhaidar <suholee75@gmail.com>
2025-05-07 07:46:36 +0700

    tambah gas

--- insert.php
+++ insert.php
@@ -3,7 +3,10 @@ require "db.php";
 $data = json_decode(file_get_contents('php://input'), true);
 $suhu = $data['esp_suhu'] ?? null;
 $kelembapan = $data['esp_kelembapan'] ?? null;
-$query = "INSERT INTO datasensor (esp_suhu, esp_kelembapan) VALUES ($suhu, $kelembapan)";
+$co = $data['co'] ?? null;
+$co2 = $data['co2'] ?? null;
+$nh4 = $data['nh4'] ?? null;
+$query = "INSERT INTO datasensor (esp_suhu, esp_kelembapan, co, co2, nh4) VALUES ($suhu, $kelembapan, $co, $co2, $nh4)";
 
 if ($koneksi->query($query)) {
     echo json_encode("✅ Datanya masuk maseh - Suhu: $suhu, Kelembapan: $kelembapan");
--- read.php
+++ read.php
@@ -1,14 +1,17 @@
 <?php
 header('Content-Type: application/json');
-header('Acces-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 include "db.php";
-$sql = mysqli_query($koneksi, "SELECT esp_suhu, esp_kelembapan, waktu FROM datasensor ORDER BY id DESC LIMIT 1");
+$sql = mysqli_query($koneksi, "SELECT esp_suhu, esp_kelembapan, co, co2, nh4, waktu FROM datasensor ORDER BY id DESC LIMIT 1");
 $data = mysqli_fetch_assoc($sql);
 
 if ($data) {
     echo json_encode([
         'temperature' => $data['esp_suhu'] . '°C',
         'humidity' => $data['esp_kelembapan'] . '%',
+        'co' => $data['co'] . ' PPM',
+        'co2' => $data['co2'] . ' PPM',
+        'nh4' => $data['nh4'] . ' PPM',
         'timestamp' => $data['waktu']
     ]);
 } else {
